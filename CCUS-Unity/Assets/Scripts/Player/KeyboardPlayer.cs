7a1e1b68a7c79d66143b60f179794e93dc1022ce
Doggitoz <cpwheeler4@gmail.com>
2022-08-13 17:32:45 -0500

    Functional Keyboard Player
    -Added camera movement to the FPS controller
    -Movement implemented

--- CCUS-Unity/Assets/Scripts/Player/KeyboardPlayer.cs
+++ CCUS-Unity/Assets/Scripts/Player/KeyboardPlayer.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEngine.InputSystem.InputAction;
+using UnityEngine.InputSystem;
 
 /**********
  * Created by: Coleton Wheeler
@@ -15,17 +15,31 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class KeyboardPlayer : MonoBehaviour
 {
-    private Input playerInput;
+    public PlayerInput playerInput;
+    public CharacterController controller;
+
+    [Space(10)]
 
     //Cursor and camera variables
+    [Header("Camera and Cursor variables")]
     [SerializeField] private Camera PlayerCamera;
-    public float mouseSensitivity = 3.5f;
-    private float rotationY;
-    private bool lockCursor = true;
+    public float mouseSensitivity = 35f;
+    private float rotationX = 0f;
+    public bool enableCameraRotate = false;
+
+    [Space(10)]
+
+    //Movement variables
+    [Header("Movement variables")]
+    public float speed = 15f;
+    private InputAction horizontal;
+    private InputAction vertical;
 
     void Start()
     {
-        
+        horizontal = playerInput.actions.FindAction("Horizontal");
+        vertical = playerInput.actions.FindAction("Vertical");
+        LockCameraRotate();
     }
 
 
@@ -39,52 +53,82 @@ public class KeyboardPlayer : MonoBehaviour
     #region Mouse Updates
     private void MouseUpdate()
     {
-        if (lockCursor)
+        if (enableCameraRotate)
         {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-
-            //Rework with new input system.
-
-            //Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-
-            //cameraPitch -= targetMouseDelta.y * mouseSensitivity;
-            //cameraPitch = Mathf.Clamp(cameraPitch, -85.0f, 85.0f);
-
-            //PlayerCamera.transform.localEulerAngles = Vector3.right * cameraPitch; // I dont know how local Euler Angles works ------ research later
-            //transform.Rotate(Vector3.up * targetMouseDelta.x * mouseSensitivity);
-        }
-        else
-        {
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
+            MoveCamera(); //Calculates mouse movement and alters the camera
         }
 
-        ////Move camera based on cursor movement
-        //rotationY += Input.GetAxis("Mouse Y") * mouseSensitivity;
-        //rotationY = Mathf.Clamp(rotationY, -70f, 70f);
-        //PlayerCamera.transform.localEulerAngles = new Vector3(-rotationY, PlayerCamera.transform.localEulerAngles.y, 0);
-        //transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0));
 
 
     }
 
-    public void MoveCamera(CallbackContext context)
+    public void MoveCamera()
     {
-        Debug.Log(context);
+        //Create variables to store the x and y movement
+        float mouseX = 0;
+        float mouseY = 0;
+
+        //Run through different input types to detect and record x and y camera movements
+        if (Mouse.current != null)
+        {
+            mouseX = Mouse.current.delta.ReadValue().x;
+            mouseY = Mouse.current.delta.ReadValue().y;
+        }
+        else if (Gamepad.current != null) //Controller compatability for later use
+        {
+            mouseX = Gamepad.current.rightStick.ReadValue().x;
+            mouseY = Gamepad.current.rightStick.ReadValue().y;
+        }
+
+        //Adjust mouse values by the sensitivity selected
+        mouseX *= mouseSensitivity;
+        mouseY *= mouseSensitivity;
+
+        //Rotates the horizontal direction
+        transform.Rotate(Vector3.up * mouseX * Time.deltaTime);
+
+        //Alters and clamps the vertical rotation
+        rotationX -= mouseY * Time.deltaTime;
+        rotationX = Mathf.Clamp(rotationX, -70f, 70f);
+        transform.localEulerAngles = new Vector3(rotationX, transform.localEulerAngles.y, 0);
+
+        
     }
 
     #endregion
 
+    #region Character Movement
     private void MovementUpdate()
     {
+        //Using Player Input actions, receive horizontal and vertical input axises
+        float x = horizontal.ReadValue<float>();
+        float z = vertical.ReadValue<float>();
 
+        //Cram the values into a vector, orientate it in respect to the camera, and then set the Y to 0
+        Vector3 move = new Vector3(x, 0, z);
+        move = PlayerCamera.transform.TransformDirection(move);
+        move.y = 0; //This can potentially cause issues later if we want vertical climbing i.e. stairs. For another day...
+
+        controller.Move(move * speed * Time.deltaTime);
     }
 
-    public void ToggleCursor()
+    #endregion
+
+    #region CameraLock methods
+    public void UnlockCameraRotate()
     {
-        lockCursor = !lockCursor;
-        Cursor.visible = lockCursor;
+        enableCameraRotate = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
+
+    public void LockCameraRotate()
+    {
+        enableCameraRotate = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    #endregion
 
 }
